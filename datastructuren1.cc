2d2b396af20b7d18c28f39e4c53c9a60f722eedf
McOkiner <b.c.r.van.starkenburg@umail.leidenuniv.nl>
2018-10-05 13:44:01 +0200

    eerste commit

---
+++ datastructuren1.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "array.h"
+#include "linkedlist.h"
+#include "stlstack.h"
+#include "stlvector.h"
+#include "backspace.h"
+
+
+int main(int, char* [])
+{
+    std::string invoer;
+    std::cin >> invoer;
+
+    std::cout << "Array Stack:        ";
+    backspace<Array<char> >(invoer);
+    std::cout << "Linked List Stack:  ";
+    backspace<LinkedList<char> >(invoer);
+    std::cout << "STL Vector Stack:   ";
+    backspace<StlVector<char> >(invoer);
+    std::cout << "STL Stack Stack:    ";
+    backspace<StlStack<char> >(invoer);
+
+    return 0;
+} // main
